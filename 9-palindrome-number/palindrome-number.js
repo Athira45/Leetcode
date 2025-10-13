1a71c557e99aa6bf20b1c92538c03b6211acea6d
Athira45 <163808989+Athira45@users.noreply.github.com>
2025-10-13 14:44:28 +0530

    Time: 9 ms (39.95%) | Memory: 65.5 MB (20.41%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,13 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if(x<0) return false;
-   let temp = x;
-    let rev = 0;
-while(temp!==0){
-   rem = temp%10;
-   rev = rev*10+rem;
-   temp = Math.floor(temp/10);
-}
-return rev === x;
+   let str = x.toString();
+   let rev = str.split('').reverse().join('')
+   return rev === str
 };
