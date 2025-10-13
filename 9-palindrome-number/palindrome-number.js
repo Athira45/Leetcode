c26ba19131cf341434d016e9240876447f45f54b
Athira45 <163808989+Athira45@users.noreply.github.com>
2025-10-13 13:58:00 +0530

    Time: 4 ms (90.21%) | Memory: 63.8 MB (71.72%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if(x<0) return false;
+   let temp = x;
+    let rev = 0;
+while(temp!==0){
+   rem = temp%10;
+   rev = rev*10+rem
+   temp = Math.floor(temp/10); 
+}
+return rev === x
+
+};
