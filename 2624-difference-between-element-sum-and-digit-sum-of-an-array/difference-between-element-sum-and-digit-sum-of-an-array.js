50f5d60e95e98e7a5f5c8c4aa441439b18f54dee
Athira45 <163808989+Athira45@users.noreply.github.com>
2025-10-16 14:18:38 +0530

    Time: 1 ms (97.47%) | Memory: 55.8 MB (85.35%) - LeetSync

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.js
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.js
@@ -9,11 +9,17 @@ let digitSum=0
 for(let i=0;i<nums.length;i++){
     elementSum+=nums[i];
     let num = nums[i];
-    while(num>0){
+    let length = nums[i].toString().length;
+    if(length==1){
+      digitSum += nums[i]
+    }else{
+     while(num>0){
      let digit = num%10;
      digitSum += digit;
      num = Math.floor(num/10);
     }
+    }
+    
 }
    return elementSum-digitSum
 };
