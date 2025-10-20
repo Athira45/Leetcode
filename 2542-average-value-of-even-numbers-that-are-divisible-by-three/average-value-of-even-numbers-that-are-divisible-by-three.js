0b77ac02b6e8745bb1e2a50a396f6af7064276e5
Athira45 <163808989+Athira45@users.noreply.github.com>
2025-10-20 11:32:31 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (93.64%) - LeetSync

--- 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.js
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.js
@@ -6,11 +6,9 @@ var averageValue = function(nums) {
     let count = 0;
     let sum = 0;
     for(let i=0; i<nums.length; i++){
-        if(nums[i]%2 == 0){
-          if(nums[i]%3 == 0){
+        if(nums[i]%2 == 0 && nums[i]%3 == 0 ){
              count++;
              sum+=nums[i];
-          }
         }
     }
     let avg = Math.floor(sum/count);
