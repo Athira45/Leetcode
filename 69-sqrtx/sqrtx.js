287a8b90b4697d01c77025f817bbe8d81633a33a
Athira45 <163808989+Athira45@users.noreply.github.com>
2025-12-01 15:43:58 +0530

    Time: 1 ms (67.92%) | Memory: 57.2 MB (11.60%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -3,6 +3,19 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    let ans =Math.floor(Math.sqrt(x))
+  let start = 0
+    let end = x
+    let ans;
+    while (start <= end)
+    {
+        let mid = Math.floor((start + end) / 2)
+        let midSquare = mid * mid
+        if(midSquare === x) return mid
+        else if(midSquare > x ) end = mid - 1
+        else if(midSquare < x){
+            start = mid + 1
+            ans = mid
+        }
+    }
     return ans
 };
