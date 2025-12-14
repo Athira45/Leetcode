eccbf1ad2bb38162b4ecc5ef6dba494003788d56
Athira45 <163808989+Athira45@users.noreply.github.com>
2025-12-14 21:40:43 +0530

    Time: 0 ms (100.00%) | Memory: 59.5 MB (73.01%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {void} 
+ */
+var moveZeroes = function(nums) {
+    let k = 0; 
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  while (k < nums.length) {
+    nums[k] = 0;
+    k++;
+  }
+};
